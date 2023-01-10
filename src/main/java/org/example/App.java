7d146f1f31df5975c7d5ee268718a2f6fa18494d
VikMoto <ig.basalt@gmail.com>
2023-01-10 20:38:53 +0200

    GradleNameTest 10.01.2023

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        Map<String, String> human = Map.of(
+                "name",
+                "Viktor",
+                "lastName",
+                "Motovilov"
+        );
+
+        Gson gson = new Gson();
+        final String json = gson.toJson(human);
+        System.out.println(json);
+    }
+
+
+}
